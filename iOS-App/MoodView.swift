799e09c6556144c71b4719cd84b678c54b8d44fe
Abhiram Ruthala <134224393+AbhiramRuthala@users.noreply.github.com>
2025-01-24 11:00:48 -0500

    Create MoodView.swift

---
+++ iOS-App/MoodView.swift
@@ -0,0 +1,22 @@
+//
+//  MoodView.swift
+//  Stability
+//
+//  Created by Abhiram Ruthala on 1/24/25.
+//
+
+import SwiftUI
+
+struct MoodView: View {
+    var body: some View {
+       
+        //This page will have a mood log system and insight-based analytics. This will show mood progression over time and give people ideas on how their mood is changing when using their device.
+        
+        
+        
+    }
+}
+
+#Preview {
+    MoodView()
+}
