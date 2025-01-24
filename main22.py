77663c6f907024296a05781fc0895c89ab1f5735
Abhiram Ruthala <134224393+AbhiramRuthala@users.noreply.github.com>
2025-01-24 11:06:17 -0500

    Delete main22.py

--- main22.py
+++
@@ -1,7 +0,0 @@
-import mediapipe as mp
-import mne
-
-
-eye_track = mp.solutions
-#eyemetric scan in analysis with brain waves
-#this gives a comprehensive outlook into not juust the mood but the general brain activity as well. 
